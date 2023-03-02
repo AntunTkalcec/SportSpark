b6f7c546c3896ef9521c7b819144b4a63230e5a5
Atkalcec <antun22596@gmail.com>
2023-03-02 20:58:59 +0100

    init commit

---
+++ SportSpark/SportSpark/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SportSpark
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
